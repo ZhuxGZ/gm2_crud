484e2d91f153b2a7a5804a34cae49fcdb259b283
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-15 11:14:58 -0300

    feat: Add card and dashboard components to the main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Dashboard from "./components/container/Dashboard";
+import ProductCard from "./components/container/Card";
 import Layout from "./components/layout/Layout";
 
+const testprod = {
+	name: "Test",
+	price: 2500,
+};
+
 const App = () => {
-	return <Layout></Layout>;
+	return (
+		<Layout>
+			<Dashboard>
+				<ProductCard name={testprod.name} price={testprod.price} />
+			</Dashboard>
+		</Layout>
+	);
 };
 
 export default App;
