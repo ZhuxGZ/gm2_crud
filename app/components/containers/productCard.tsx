00a0a88fd7e656fe85d580a1376aa82b5319155e
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-09-07 12:56:01 -0300

    feat(productCards): Add dropdown menu

--- app/components/containers/productCard.tsx
+++ app/components/containers/productCard.tsx
@@ -1,22 +1,42 @@
 import { Settings } from '@mui/icons-material';
-import { Button, Card } from '@mui/joy';
+import { Button, Card, Dropdown, Menu, MenuButton, MenuItem } from '@mui/joy';
+import Image from 'next/image';
+import DeleteProdModal from './Modals/DeleteProdModal';
+import EditProdModal from './Modals/EditProdModal';
 
 const ProductCard = () => {
 	return (
-		<Card variant="solid" sx={{ height: '20em' }}>
-			<div className="prodInfo">
-				<img
+		<Card variant="solid">
+			<div className="prodImg">
+				<Image
 					src="https://http2.mlstatic.com/D_NQ_NP_2X_648428-MLA46516517286_062021-F.webp"
-					alt=""
-					width="300px"
+					width={300}
+					height={250}
+					alt="Mackbook photo"
 				/>
-				<h3 className="title font-bold text-xl mt-7">Mackbook</h3>
 			</div>
-			<div className="cardBottom flex items-center mr-2">
-				<p className="price text-lg">$2500</p>
-				<Button sx={{ marginLeft: 'auto' }} color="neutral">
-					<Settings sx={{ fill: '#FF8645' }} />
-				</Button>
+			<div className="cardBottom flex mr-2 mt-4 mb-2 justify-center items-center">
+				<div className="prodInfo">
+					<h3 className="title font-bold text-xl ">Mackbook</h3>
+					<p className="price text-lg">$2500</p>
+				</div>
+				<Dropdown>
+					<MenuButton
+						variant="plain"
+						sx={{ marginLeft: 'auto', padding: 1 }}
+						color="neutral"
+					>
+						<Settings sx={{ fill: '#FF8645' }} />
+					</MenuButton>
+					<Menu keepMounted sx={{ padding: 0 }}>
+						<MenuItem sx={{ padding: '1px 1px' }}>
+							<DeleteProdModal />
+						</MenuItem>
+						<MenuItem sx={{ padding: '1px 1px' }}>
+							<EditProdModal />
+						</MenuItem>
+					</Menu>
+				</Dropdown>
 			</div>
 		</Card>
 	);
