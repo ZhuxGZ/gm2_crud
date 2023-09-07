116d2aab87fb987eb6705e75376d57cabaf16981
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-09-07 12:49:32 -0300

    feat(ProductDisplay): Add getProducts() function

--- app/components/containers/ProductDisplay.tsx
+++ app/components/containers/ProductDisplay.tsx
@@ -1,6 +1,11 @@
 import { Grid } from '@mui/joy';
 import { ReactNode } from 'react';
 import ProductCard from './productCard';
+const getProducts = async () => {
+	const API_URL = '';
+	const response = await fetch(API_URL);
+	const data = await response.json();
+};
 
 const ProductDisplay = () => {
 	return (
