4742153025ad0fababdb33103f79933f77cc7534
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-09-07 12:57:41 -0300

    feat: Add the button to add products

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,16 @@
 import { Add } from '@mui/icons-material';
 import { Button, Divider } from '@mui/joy';
 import ProductDisplay from '../components/containers/ProductDisplay';
+import AddProdModal from '../components/containers/Modals/AddProdModal';
 
 const Dashboard = () => {
 	return (
 		<>
 			<section id="header" className="m-8 mb-4 flex items-center">
 				<h1 className="font-bold text-xl">Dashboard</h1>
-				<Button color="success" startDecorator={<Add />} sx={{ marginLeft: 'auto' }}>
-					Add
-				</Button>
+				<div className="ml-auto">
+					<AddProdModal />
+				</div>
 			</section>
 			<Divider sx={{ marginLeft: '1.5rem', marginRight: '1.5rem' }} />
 			<ProductDisplay />
